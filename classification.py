55236faa4728c071b81d30c7c52ece64e4848db0
samir-fawzy <sfawzy862@gamil.com>
2025-02-26 17:59:11 +0200

    finish task

--- classification.py
+++ classification.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sc
 ############### data ###############
-path = "C:\ML\Classification\First_Project_Classification\data.txt"
+path = "C:\\ML\\Classification\\First_Project_Classification\\data.txt"
 data = pd.read_csv(path,header= None,names=["Exam1","Exam2","Admitted"])
 # print(data)
 
@@ -12,8 +12,8 @@ data.insert(0,"Bais",1)
 
 # separate featurs and output 
 col =data.shape[1] # output number of columns for data 
-x = data.iloc[:,0:col - 1]
-y = data.iloc[:,col - 1 : col]
+x = data.iloc[ : , 0 : col - 1]
+y = data.iloc[ : , col - 1 : col]
 
 # print("x\n",x)
 # print("="*50)
@@ -34,23 +34,27 @@ negative = data[data["Admitted"].isin([0])]
 # plt.show()
 
 # convert x , y to matrix
-x_matrix = np.matrix(x.values)
-y_matrix = np.matrix(y.values)
-theta = np.zeros(x_matrix.shape[1])
+x = np.matrix(x.values)
+y= np.matrix(y.values)
+theta = np.zeros((x.shape[1],1))
 
-# print(x_matrix)
-# print(y_matrix) 
-# print(theta.shape)
+# print(x)
+# print(f"y shape : {y.shape}") 
+# print(f"x shape : {x.shape}")
+# print(f"theta shape : {theta.shape}")
 
 # implement Sigmoid Function
 def Sigmoid(z):
     return 1 / (1 + np.exp(-z))
+# test = np.log(Sigmoid(x @ theta))
+# print (f"test {test.shape}")
 
 #implement Cost Function
 def Cost(theta,x, y):
-    m = len(y)
+    m = len(x)
     prediction = Sigmoid(x @ theta)
-    cost = (-1/m) * (np.sum(y @ np.log(prediction) + (1 - y)@(np.log(1 - prediction))))
+    epsilon = 1e-5
+    cost = (-1/m) * (np.sum(np.multiply(y, np.log(prediction + epsilon).T) + np.multiply((1 - y), np.log(1 - prediction + epsilon)).T))
     return cost
 
 # implement Gradiant Function
@@ -63,20 +67,49 @@ def Gradient(theta,x,y):
     grad = (1/m) * (x.T @ error)
     return grad
 
-cost = Cost(theta,x_matrix,y_matrix)
-print(f"Cost = {cost}")
+result = sc.optimize.fmin_tnc(func=Cost, x0=theta, args=(x, y), fprime=Gradient)
 
-result = sc.optimize.fmin_tnc(func=Cost, x0=theta, args=(x_matrix, y_matrix), fprime=Gradient)
 
-# optimize_theta = result[0]
-# print(result)
-# print(optimize_theta)
+newTheta = result[0]
 
-# test = Sigmoid(x_matrix @ optimize_theta)
-# c = Cost(optimize_theta,x_matrix,y_matrix)   
-# print(c)
+print(f"newtheta : {newTheta.shape}")
+print(f"x : {x.shape}")
+print(f"y : {y.shape}")
 
-sa = Gradient(theta,x_matrix,y_matrix)  
-print("------------------",sa.shape)
-print(x_matrix.shape    )
-cost = Cost(sa,x_matrix,y_matrix)
+
+
+# print(f"newTheta :\n{newTheta}")
+# print("============================")
+cost0 = Cost(theta,x,y)
+
+print(f"Cost Before = {cost0}")
+print("============================")
+cost1 = Cost(newTheta,x,y)
+
+print(f"Cost After: {cost1}")
+
+def predict(x, theta):
+    p = Sigmoid(x @ newTheta)
+    for i in range(96):
+        if p[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+    return p
+
+t = predict(x,newTheta)
+
+print(t.reshape(96,1))
+
+# p = Sigmoid(x @ newTheta)
+
+# print(p.shape)
+
+# for i in range(96):
+#     if p[0,i] > 0.5:
+#         p[0,i] = 1
+#     else:
+#         p[0,i] = 0
+
+# print(p)
+
